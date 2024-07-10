9f02efe7cba1235ff004d8e8e6667eaf447123a9
masahiro1211 <masahiro200312@gmail.com>
2024-07-10 17:26:31 +0900

    randomforestを含めてtrainの向上を行いました

--- src/train.py
+++ src/train.py
@@ -1,6 +1,8 @@
 import os
 import sys
-project_root_path = os.path.dirname(os.path.dirname( os.path.abspath(__file__) )) 
+import numpy as np
+from sklearn.ensemble import VotingClassifier
+project_root_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__))) 
 sys.path.append(project_root_path)
 import my_library.Trainer as Trainer
 
@@ -10,8 +12,28 @@ model_dump_path_base = input("Enter the path for the directory to save the model
 trainer = Trainer.Trainer()
 X, y = trainer.load_data(preprocessed_train_data_path)
 
-hyperparameters = [0.1,1,10,100,1000]
-for h in hyperparameters:
-    model_dump_path = os.path.join(model_dump_path_base , "SVM_"+str(h)+".pkl") 
+# SVMモデルのトレーニング
+svm_hyperparameters = [0.1, 1, 10, 100, 1000]
+svm_models = []
+for h in svm_hyperparameters:
     trainer.train_SVM(h, X, y)
-    trainer.dump_model(model_dump_path)
+    svm_models.append(trainer.model)
+
+# ランダムフォレストモデルのトレーニング
+rf_hyperparameters = [5, 10, 15, 20, 25]
+rf_models = []
+for h in rf_hyperparameters:
+    trainer.train_RF(h, X, y)
+    rf_models.append(trainer.model)
+
+# アンサンブルモデルの作成
+ensemble_estimators = [('svm_' + str(h), model) for h, model in zip(svm_hyperparameters, svm_models)] + \
+                      [('rf_' + str(h), model) for h, model in zip(rf_hyperparameters, rf_models)]
+
+ensemble_model = VotingClassifier(estimators=ensemble_estimators, voting='soft')
+ensemble_model.fit(X, y)
+
+# アンサンブルモデルの保存
+ensemble_model_path = os.path.join(model_dump_path_base, "ensemble_model.pkl")
+trainer.model = ensemble_model
+trainer.dump_model(ensemble_model_path)
