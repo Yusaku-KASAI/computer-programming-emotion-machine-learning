c90ec77953d1e8e62e1a20f914e1d7e4859f432d
masahiro1211 <masahiro200312@gmail.com>
2024-07-10 21:34:35 +0900

    修正

--- src/my_library/count_statistics.py
+++ src/my_library/count_statistics.py
@@ -1,9 +1,4 @@
-#入力された文章 sent に含まれる単語を感情辞書 dic と照合し、各感情（ポジティブ、ネガティブ、ニュートラル）の単語数をカウントしてベクトルとして返す関数です。
 from janome.tokenizer import Tokenizer
-
-NEGATIVE = "n"
-POSITIVE = "p"
-NEUTRAL = "e"
 
 def count_and_vectorize(dic, sent):
     tokenizer = Tokenizer()
@@ -14,17 +9,14 @@ def count_and_vectorize(dic, sent):
     for token in tokenizer.tokenize(sent):
         words.append(token.base_form)
 
-    # 辞書を単語をキーとして検索
-    for word in words:
-        if word in dic:
-            emotion = dic[word]
-            if emotion == NEGATIVE:
+    for i in range(len(dic)):        
+        if dic[i][0] in words:
+            if dic[i][1] == "n":
                 num_negative += 1
-            elif emotion == POSITIVE:
+            elif dic[i][1] == "p":
                 num_positive += 1
-            elif emotion == NEUTRAL:
+            elif dic[i][1] == "e":
                 num_neutral += 1
-
     num_total = num_negative + num_positive + num_neutral 
     
     return [num_positive, num_negative, num_neutral, num_total]
